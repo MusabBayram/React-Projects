3a15c6f8ed7af0f086d8e4187180e6066ea63ea0
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2022-01-17 13:58:26 +0300

    content is complete

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -15,29 +15,43 @@ function TodoList() {
     setTodos(newTodos);
   };
 
-  const removeTodo = (id) => {
-    const removeArr = [...todos].filter((todo) => todo.id !== id);
+   const updateTodo = (todoId, newValue) => {
+     if (!newValue.text || /^\s*$/.test(newValue.text)) {
+       return;
+     }
+     setTodos((prev) =>
+       prev.map((item) => (item.id === todoId ? newValue : item))
+     );
+   };
 
-    setTodos(removeArr);
-  };
+   const removeTodo = (id) => {
+     const removeArr = [...todos].filter((todo) => todo.id !== id);
 
-  const completeTodo = (id) => {
-    let updateTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        todo.isComplete = !todo.isComplete;
-      }
-      return todo;
-    });
-    setTodos(updateTodos);
-  };
+     setTodos(removeArr);
+   };
 
-  return (
-    <div>
-      <h1>What's the Plan for Today?</h1>
-      <TodoForm onSubmit={addTodo} />
-      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
-    </div>
-  );
+   const completeTodo = (id) => {
+     let updateTodos = todos.map((todo) => {
+       if (todo.id === id) {
+         todo.isComplete = !todo.isComplete;
+       }
+       return todo;
+     });
+     setTodos(updateTodos);
+   };
+
+   return (
+     <div>
+       <h1>What's the Plan for Today?</h1>
+       <TodoForm onSubmit={addTodo} />
+       <Todo
+         todos={todos}
+         completeTodo={completeTodo}
+         removeTodo={removeTodo}
+         updateTodo={updateTodo}
+       />
+     </div>
+   );
 }
 
 export default TodoList;
