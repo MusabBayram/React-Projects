e207aab460e2ce0f287c1af86c9cf07a9593c707
Musab <musab.bayram@hotmail.com>
2022-02-02 20:49:50 +0300

    Card component added

---
+++ weater-app/src/components/Card/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import styles from './index.module.css';
+
+function Card() {
+ 
+  return <div className={styles.container}>asdasd</div>;
+}
+
+export default Card;
--- weater-app/src/components/List/index.js
+++ weater-app/src/components/List/index.js
@@ -1,11 +1,24 @@
 import { useContext } from 'react';
 
+import Card from '../Card';
+import styles from './index.module.css';
 import WeatherContext from '../../context/WeatherContext';
 
 function List() {
-  const data = useContext(WeatherContext);
-  console.log(data);
-  return <div>List</div>;
+  const { weather } = useContext(WeatherContext);
+  console.log(weather);
+  return (
+  <div>
+    <pre>{JSON.stringify(weather?.city, null, 2)}</pre>
+    
+    <div className={styles.ListContainer}>
+      {weather?.list.map((day, index) => (
+        <Card key={index}/>
+      ))}
+    </div>
+    
+
+  </div>);
 }
 
 export default List;
