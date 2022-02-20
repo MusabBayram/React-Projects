c8b42373c153251ce3af6941cf259a4f683de13f
Musab <musab.bayram@hotmail.com>
2022-02-20 21:18:45 +0300

    apollo added

--- voting-app/src/App.js
+++ voting-app/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import New from "./pages/New";
+import Questions from "./pages/Questions";
+import QuestionDetail from "./pages/QuestionDetail";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app">
+			<Router>
+				<div>
+					<nav>
+						<ul className="menu">
+							<li>
+								<Link to="/">Questions</Link>
+							</li>
+							<li>
+								<Link to="/new">New</Link>
+							</li>
+						</ul>
+					</nav>
+					<hr />
+
+					<div className="content">
+						<Switch>
+							<Route path="/" exact component={Questions} />
+							<Route path="/q/:id" component={QuestionDetail} />
+							<Route path="/new" component={New} />
+						</Switch>
+					</div>
+				</div>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
