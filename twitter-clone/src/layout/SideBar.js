9bfbf713c2ce865346db6ecb38e77f8cd1f523b6
Musab Bayram <musab.bayram@hotmail.com>
2022-02-28 15:51:34 +0300

    icons added

--- twitter-clone/src/layout/SideBar.js
+++ twitter-clone/src/layout/SideBar.js
@@ -1,8 +1,29 @@
 import React from 'react'
 
+import {
+    BookmarksIcon,
+    ExploreIcon,
+    HomeIcon,
+    ListsIcon,
+    MessagesIcon,
+    MoreIcon,
+    NotificationsIcon,
+    ProfileIcon,
+  } from "../icons/Icon";
+
 function SideBar() {
   return (
-    <div className="w-72 bg-blue-200"><h1>SideBar</h1></div>
+    <div className="w-72 bg-blue-200">
+        <h1>SideBar</h1>
+        <BookmarksIcon/>
+        <ExploreIcon/>
+        <HomeIcon/>
+        <ListsIcon/>
+        <MessagesIcon/>
+        <MoreIcon/>
+        <NotificationsIcon/>
+        <ProfileIcon/>
+    </div>
   )
 }
 
